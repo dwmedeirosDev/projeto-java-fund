7ae94309183a4154608081c8201fd59fd2cfe555
David Medeiros <davidmedeiros94@hotmail.com>
2024-12-17 23:33:08 -0300

    Criação de pacote & classe

--- src/main/java/fundamentos/Medidas.java
+++ src/main/java/fundamentos/Medidas.java
@@ -1,7 +1,20 @@
+// 1- Pacote
 package fundamentos;
 
-public class Medidas {
-    public static void main(String[] args){
+// 2- Bibliotecas
 
+// 3- Classe
+public class Medidas {
+    //3.1- Atributos
+
+    //3.2- Métodos e Funções
+    public static void main(String[] args){
+        System.out.println("Escolha o cálculo desejado: ");
+        System.out.println("(1) Área do quadrado");
+        System.out.println("(2) Área do retângulo");
+        System.out.println("(3) Área do triângulo");
+        System.out.println("(4) Área do círculo");
     }
+
+    public static int
 }
