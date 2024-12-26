018b43af61aee2d720d1a5e80d25398683a378c1
David Medeiros <davidmedeiros94@hotmail.com>
2024-12-26 15:18:54 -0300

    Criado uma nova classe e utilizado métodos como herança

---
+++ src/main/java/fundamentos/Calculos.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+import static fundamentos.Medidas.entrada;
+import static fundamentos.Medidas.tabuada;
+
+public class Calculos {
+    public static void main(String[] args){
+        System.out.print("Deseja calcular a tabuada? S/N");
+        String resposta = entrada.nextLine();
+
+        switch (resposta){
+            case "S":
+            case "s":
+                tabuada();
+                break;
+            case "N":
+            case "n":
+                System.out.println("Você saiu!");
+                break;
+            default:
+                System.out.println("Você digitou um número diferente");
+        }
+
+
+    }
+}
